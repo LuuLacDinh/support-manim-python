da38ae1a5043eab56e657b823858feba79d4c7a9
Lưu Lưu <luulacdinh.hp2021@mgail.com>
2025-07-27 14:22:14 +0700

    Add all Manim support files

--- saveinGit.py
+++ saveinGit.py
@@ -4,8 +4,8 @@ import os
 repo_path = r"D:\Py"
 commands = [
     'git init',
-    'git add a.py saveinGit.py support.py test.py',
-    'git commit -m "Add support file for Manim"',
+    'git add .',  # thêm tất cả bao gồm thư mục support/
+    'git commit -m "Add all Manim support files"',
     'git branch -M main',
     'git remote add origin https://github.com/LuuLacDinh/support-manim-python.git',
     'git push -u origin main'
@@ -13,15 +13,3 @@ commands = [
 
 for cmd in commands:
     sub.run(cmd, cwd=repo_path, shell=True)
-    
-repo_path2 = r"D:\Py\support"
-commands = [
-    'git init',
-    'git add axes.py effect.py transform.py',
-    'git commit -m "Add support file for Manim"',
-    'git branch -M main',
-    'git remote add origin https://github.com/LuuLacDinh/support-manim-python.git',
-    'git push -u origin main'
-]
-for cmd in commands:
-    sub.run(cmd, cwd=repo_path2, shell=True)
